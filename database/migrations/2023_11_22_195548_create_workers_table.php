740f4cb9602be2af53f108cf9b2e435e0d408fe6
den <vomehad@yandex.ru>
2023-11-22 23:04:32 +0300

    init | migration

---
+++ database/migrations/2023_11_22_195548_create_workers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('position_id')->constrained('dict_positions')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('branch_id')->constrained('branchs')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workers');
+    }
+};
